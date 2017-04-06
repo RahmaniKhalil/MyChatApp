9fdbf254951b516728a942adc88db533411933c1
Khalil RAHMANI <khalil.rahmani.pro@gmail.com>
2017-04-06 14:21:29 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCCliv2y74eM_gaIEoVaKYUo78WKns9hco",
+    authDomain: "mychatapp-52748.firebaseapp.com",
+    databaseURL: "https://mychatapp-52748.firebaseio.com",
+    projectId: "mychatapp-52748",
+    storageBucket: "mychatapp-52748.appspot.com",
+    messagingSenderId: "388960783528"
+
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
